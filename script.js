5c309d7d6e10007dd1d5fcbbabba6ff6c84aba2d
K M Saddam Hossain <dev.saddam@gmail.com>
2022-12-07 12:16:28 +0600

    Functions

---
+++ script.js
@@ -0,0 +1,51 @@
+//console.log(`Test.........`);
+/*
+Project: TODO Task 
+Technology: HTML, CSS, Vanila JS
+Author: K M Saddam Hossain
+Author Url: kmsaddam.com
+*/
+
+//DOM Initials
+let form = document.querySelector("form");
+let newTask = document.querySelector("#new-task");
+let incompleteUI = document.querySelector(".incomplete-task");
+let completeUI = document.querySelector(".complete-task");
+
+//functions
+
+let addTask = (task) => {
+  let list = document.createElement("li");
+  list.className = "list-group-item";
+
+  let checkbox = document.createElement("input");
+  checkbox.type = checkbox;
+  checkbox.onchange = completeTask(task);
+
+  let label = document.createElement("label");
+  label.textContent = task;
+
+  list.appendChild(checkbox);
+  list.appendChild(label);
+
+  return list;
+};
+
+let completeTask = (task) => {
+  let list = document.createElement("li");
+  list.className = "list-group-item";
+
+  let label = document.createElement("label");
+  label.textContent = task;
+
+  let btn = document.createElement("button");
+  btn.className = "btn btn-danger";
+  btn.textContent = "Delete";
+
+  list.appendChild(label);
+  list.appendChild(btn);
+
+  return list;
+};
+
+console.log(form);
